fa26b1a185599e64eb35177652f2f087aff41e76
Andres Correa Casablanca <castarco@coderspirit.xyz>
2022-02-06 10:17:58 +0100

    test: cover container's (aparent) immutability

--- lambda-ioc/src/__tests__/container.test.ts
+++ lambda-ioc/src/__tests__/container.test.ts
@@ -41,4 +41,37 @@ describe('container', () => {
 
     expect(await container.resolveAsync('ab')).toBe(15)
   })
+
+  it('can re-register sync dependencies (without changing the "original" container)', async () => {
+    const c1 = createContainer()
+      .register('a', () => 3)
+      .register('b', () => 5)
+      .registerAsync('c', async () => await Promise.resolve(11))
+      .register('ab', (c) => c.resolve('a') * c.resolve('b'))
+      .registerAsync(
+        'ac',
+        async (c) => c.resolve('a') * (await c.resolveAsync('c')),
+      )
+    const c2 = c1.register('a', () => 7)
+
+    expect(c1.resolve('ab')).toBe(15)
+    expect(c2.resolve('ab')).toBe(35)
+
+    expect(await c1.resolveAsync('ac')).toBe(33)
+    expect(await c2.resolveAsync('ac')).toBe(77)
+  })
+
+  it('can re-register async dependencies (without changing the "original" container)', async () => {
+    const c1 = createContainer()
+      .register('a', () => 3)
+      .registerAsync('b', async () => await Promise.resolve(5))
+      .registerAsync(
+        'ab',
+        async (c) => c.resolve('a') * (await c.resolveAsync('b')),
+      )
+    const c2 = c1.registerAsync('b', async () => await Promise.resolve(7))
+
+    expect(await c1.resolveAsync('ab')).toBe(15)
+    expect(await c2.resolveAsync('ab')).toBe(21)
+  })
 })
